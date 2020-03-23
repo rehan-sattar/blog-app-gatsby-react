77e634ddc1e3f4e70756dbbcd42676cd6085b05c
rehan-sattar <rehansattar117@gmail.com>
2020-03-24 04:31:41 +0500

    feat(cms) -  add cms to application

--- gatsby-config.js
+++ gatsby-config.js
@@ -10,6 +10,13 @@ module.exports = {
     author: "Rehan Sattar",
   },
   plugins: [
+    {
+      resolve: `gatsby-source-contentful`,
+      options: {
+        spaceId: process.env.CONTENTFUL_SPACE_ID,
+        accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
+      },
+    },
     "gatsby-plugin-sass",
     {
       resolve: `gatsby-source-filesystem`,
