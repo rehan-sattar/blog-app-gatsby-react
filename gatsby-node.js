57790a936f60a484a2a4d95a349902ec9dccb76e
rehan-sattar <rehansattar117@gmail.com>
2020-03-24 02:45:36 +0500

    feat(blogs) - generate dynamic blog with respect to slug

--- gatsby-node.js
+++ gatsby-node.js
@@ -12,3 +12,37 @@ module.exports.onCreateNode = ({ node, actions }) => {
     })
   }
 }
+
+/**
+ * function for creating dynamic pages
+ */
+
+module.exports.createPages = async ({ graphql, actions }) => {
+  const { createPage } = actions
+  const pathToBlogTemplate = path.resolve("./src/templates/blog.js")
+
+  const res = await graphql(`
+    query {
+      allMarkdownRemark {
+        edges {
+          node {
+            fields {
+              slug
+            }
+          }
+        }
+      }
+    }
+  `)
+  res.data.allMarkdownRemark.edges.forEach(edge => {
+    console.log("module.exports.createPages -> edge", edge)
+
+    createPage({
+      path: `/blog/${edge.node.fields.slug}`,
+      component: pathToBlogTemplate,
+      context: {
+        slug: edge.node.fields.slug,
+      },
+    })
+  })
+}
--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,22 +1,8 @@
 import React from "react"
 import Layout from "../components/layout"
-import { graphql, useStaticQuery } from "gatsby"
+import { graphql, useStaticQuery, Link } from "gatsby"
 
 const BlogPage = () => {
-  // static data of blogs.
-  const blogs = [
-    {
-      title: "Understanding Redux, What ? When? Why ?",
-      link:
-        "https://medium.com/@rehansattar/understanding-redux-what-when-why-741178293ac1",
-    },
-    {
-      title: "ROAD MAP OF A FRONT END DEVELOPER, What I have to learn?",
-      link:
-        "https://medium.com/@rehansattar/road-map-of-a-front-end-developer-what-i-have-to-learn-892e67fcd3b9",
-    },
-  ]
-
   const data = useStaticQuery(graphql`
     query {
       allMarkdownRemark {
@@ -26,6 +12,9 @@ const BlogPage = () => {
             frontmatter {
               title
               date
+            }
+            fields {
+              slug
             }
           }
         }
@@ -37,31 +26,22 @@ const BlogPage = () => {
     html: e.node.html,
     title: e.node.frontmatter.title,
     date: e.node.frontmatter.date,
+    slug: e.node.fields.slug,
   }))
 
   console.log("POSTS", posts)
   return (
     <Layout>
       <h1>Blogs</h1>
-      {blogs.map((blog, index) => (
-        <div key={index}>
-          <h5>
-            {index + 1}. {blog.title}
-          </h5>
-          <p>
-            <a href={blog.link}>Read this blog here.</a>
-          </p>
-        </div>
-      ))}
-
-      <h1>Posts</h1>
-      {posts.map((post, index) => (
-        <div key={index}>
-          <h5>
-            {index + 1}. {post.title}
-          </h5>
-        </div>
-      ))}
+      <ol>
+        {posts.map((post, index) => (
+          <li>
+            <h2>
+              <Link to={`/blog/${post.slug}`}>{post.title}</Link>
+            </h2>
+          </li>
+        ))}
+      </ol>
     </Layout>
   )
 }
